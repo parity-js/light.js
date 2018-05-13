c531c68b5268a1bf7aba1f43424f440b2bde7828
Amaury Martiny <amaury.martiny@gmail.com>
2018-05-13 17:57:16 +0200

    Add headers

--- example/src/App.js
+++ example/src/App.js
@@ -1,3 +1,7 @@
+// Copyright 2015-2018 Parity Technologies (UK) Ltd.
+// This file is part of Parity.
+//
+// SPDX-License-Identifier: MIT
 import React, { Component } from 'react';
 
 import logo from './logo.svg';
